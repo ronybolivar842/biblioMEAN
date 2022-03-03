85b2e9e4f7359002378b663a3c4305e05051918b
ronybolivar842 <rony-bolivar-842@hotmail.com>
2022-03-03 10:44:10 -0500

    second commit

--- backend/middleware/userValidations.js
+++ backend/middleware/userValidations.js
@@ -1,23 +1,19 @@
 import model from "../models/user.js";
 
-const validations = (req, res, next) => {
-  validData(req, res);
-  existingEmail(req, res, next);
-};
-
 const validData = (req, res, next) => {
   if (!req.body.name || !req.body.email || !req.body.password)
     return res.status(400).send({ message: "Incomplete Data" });
-  if (next) next();
+
+  next();
 };
 
 const existingEmail = async (req, res, next) => {
   const userFound = await model.findOne({ email: req.body.email });
 
-  if (userFound)
+  if (userFound && req.body._id != userFound._id)
     return res.status(400).send({ message: "Email already exists" });
 
-  if (next) next();
+  next();
 };
 
-export default { validations, validData, existingEmail };
+export default { validData, existingEmail };
